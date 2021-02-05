d278a0d7fdee6471e11397f6ef35354a3bd54b01
Seth Peasley <seth.peasley@gmail.com>
2021-02-05 15:15:12 -0500

    Added the Swerve Subsytem components.

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,6 +4,10 @@
 
 package frc.robot;
 
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import org.opencv.core.Mat;
+
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
  * constants. This class should not be used for any other purpose. All constants should be declared
@@ -14,5 +18,74 @@ package frc.robot;
  */
 public final class Constants 
 {
+
+
+
+
+
+
+    public static final class SwerveDriveModuleConstants
+    {
+        public static final double k_MaxModuleAngularSpeedRadiansPerSecond = 2 * Math.PI;
+        public static final double k_MaxModuleAngularAccelerationRadiansPerSecondSquared = 2 * Math.PI;
+
+        public static final double k_SteeringEncoderCoefficient = (Math.PI * 2) / 4096.0; // this is 2π radians, dividided by the 12-bit encoder precision 
+
+
+        public static final int k_RevNEOEncoderCtsPerRev = 42; // the NEO's hall-effect encoder is 42 counts/rev
+        public static final double k_WheelDiameterMeters = 0.15;
+        public static final double k_MK3SwerveModuleGearRatio = 6.86;
+        public static final double k_DriveEncoderDistancePerPulse =
+            // Assumes the encoders are directly mounted on the wheel shafts
+            (k_WheelDiameterMeters * Math.PI) / ( (double) k_RevNEOEncoderCtsPerRev * 6.86);
     
+        public static final double kTurningEncoderDistancePerPulse =
+            // Assumes the encoders are on a 1:1 reduction with the module shaft.
+            (2 * Math.PI) / (double) k_RevNEOEncoderCtsPerRev;
+
+        public static final class CANBusIDs
+        {
+            public static final int k_SwerveRightFront_Drive = 1;
+            public static final int k_SwerveRightFront_Turning = 2;
+    
+            public static final int k_SwerveLeftFront_Drive = 3;
+            public static final int k_SwerveLeftFront_Turning = 4;
+    
+            public static final int k_SwerveRightRear_Drive = 5;
+            public static final int k_SwerveRightRear_Turning = 6;
+    
+            public static final int k_SwerveLeftRear_Drive = 7;
+            public static final int k_SwerveLeft_Turning = 8;
+        }
+        
+        public static final class MotorTypes
+        {
+            public static final MotorType k_SwerveRightFront_Drive = MotorType.kBrushless;
+            public static final MotorType k_SwerveRightFront_Turning = MotorType.kBrushless;
+            
+            public static final MotorType k_SwerveLeftFront_Drive = MotorType.kBrushless;
+            public static final MotorType k_SwerveLeftFront_Turning = MotorType.kBrushless;
+    
+            public static final MotorType k_SwerveRightRear_Drive = MotorType.kBrushless;
+            public static final MotorType k_SwerveRightRear_Turning = MotorType.kBrushless;
+    
+            public static final MotorType k_SwerveLeftRear_Drive = MotorType.kBrushless;
+            public static final MotorType k_SwerveLeftRear_Turning = MotorType.kBrushless;
+        }
+
+        public static final class TurningController
+        {
+            public static final double k_Prop = 1;
+            public static final double k_Inter = 0;
+            public static final double k_Diff = 0;
+        }
+
+        public static final class DriveModule
+        {
+            public static final double k_Proportional = 1;
+            public static final double k_Intergral = 0;
+            public static final double k_Differential = 0;
+        }
+
+    }
 }
---
+++ src/main/java/frc/robot/subsystems/SwerveModule.java
@@ -0,0 +1,132 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.revrobotics.CANEncoder;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import com.ctre.phoenix.sensors.CANCoder;
+import com.ctre.phoenix.sensors.CANCoderConfiguration;
+import com.ctre.phoenix.sensors.CANCoderFaults;
+import com.ctre.phoenix.sensors.CANCoderStickyFaults;
+import com.ctre.phoenix.sensors.MagnetFieldStrength;
+
+import edu.wpi.first.wpilibj.controller.PIDController;
+import edu.wpi.first.wpilibj.controller.ProfiledPIDController;
+import edu.wpi.first.wpilibj.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.kinematics.SwerveModuleState;
+import edu.wpi.first.wpilibj.trajectory.TrapezoidProfile;
+import frc.robot.Constants.SwerveDriveModuleConstants;
+
+
+
+/** 
+ * Swerve Module is based on the WPI Swerve module code.
+ * 
+ */
+public class SwerveModule
+{
+    // REV robotics Spark Max motor controllers
+    private final CANSparkMax m_driveMotor;
+    private final CANSparkMax m_steeringMotor;
+
+    // REV Robotics Spark Max encoder signal
+    private final CANEncoder m_driveEncoder;
+
+    // CTRE CANCoder encoder signal
+    private final CANCoder m_steeringEncoder;
+
+    private final PIDController m_driverPIDController = new PIDController(SwerveDriveModuleConstants.DriveModule.k_Proportional, 
+                                                                          SwerveDriveModuleConstants.DriveModule.k_Intergral,
+                                                                          SwerveDriveModuleConstants.DriveModule.k_Differential);
+                                                                          
+    private final ProfiledPIDController m_turningPIDController = 
+        new ProfiledPIDController(
+            SwerveDriveModuleConstants.TurningController.k_Prop,
+            SwerveDriveModuleConstants.TurningController.k_Inter,
+            SwerveDriveModuleConstants.TurningController.k_Diff,
+            new TrapezoidProfile.Constraints(
+                SwerveDriveModuleConstants.k_MaxModuleAngularSpeedRadiansPerSecond,
+                SwerveDriveModuleConstants.k_MaxModuleAngularAccelerationRadiansPerSecondSquared) );
+    
+    /**
+     * Constructs a SwerveModule.
+     *
+     * @param driveMotorChannel ID for the drive motor.
+     * @param turningMotorChannel ID for the turning motor.
+     */
+    public SwerveModule(
+        int driveMotorChannel,
+        MotorType driveMotorType,
+        int turningMotorChannel,
+        MotorType turningMotorType,
+        //int[] driveEncoderPorts,
+        int steeringEncoderChannel,
+        boolean driveEncoderReversed,
+        boolean turningEncoderReversed)
+        {
+            m_driveMotor = new CANSparkMax(driveMotorChannel, driveMotorType);
+            m_steeringMotor = new CANSparkMax(turningMotorChannel, turningMotorType);
+
+            this.m_driveEncoder = m_driveMotor.getEncoder();   //assumes the use of the internal motor encoder
+            m_driveEncoder.setVelocityConversionFactor(SwerveDriveModuleConstants.k_DriveEncoderDistancePerPulse); 
+
+
+            // Steering encoder is a Cross-The-Road Electronics CANCoder
+            this.m_steeringEncoder = new CANCoder(steeringEncoderChannel); 
+            
+            CANCoderConfiguration m_canCoderConfiguration = new CANCoderConfiguration();
+            m_canCoderConfiguration.unitString = "radians";
+            m_canCoderConfiguration.sensorCoefficient = SwerveDriveModuleConstants.k_SteeringEncoderCoefficient;
+
+            m_steeringEncoder.configAllSettings(m_canCoderConfiguration);
+
+
+            
+            // Limit the PID Controller's input range between -pi and pi and set the input
+            // to be continuous.
+            m_turningPIDController.enableContinuousInput(-Math.PI, Math.PI);
+        }
+
+    /**
+     * Returns the current state of the module.
+     *
+     * @return The current state of the module.
+     */
+    public SwerveModuleState getState()
+    {
+        
+        return new SwerveModuleState(m_driveEncoder.getVelocity(), // the getVelocity() has been scaled to go from RPM to m/s
+                                     new Rotation2d(m_steeringEncoder.getPosition()));
+    }
+
+    /**
+     * Sets the desired state for the module.
+     *
+     * @param state Desired state with speed and angle.
+     */
+    public void setDesiredState(SwerveModuleState state)
+    {
+        // Calculate the drive output from the drive PID controller.
+        final var driveOutput = m_driverPIDController.calculate(m_driveEncoder.getVelocity(), state.speedMetersPerSecond);
+
+        // Calculate the turning motor output from the turning PID controller.
+        final var steerOutput = m_turningPIDController.calculate(m_steeringEncoder.getPosition(), state.angle.getRadians());
+
+        m_driveMotor.set(driveOutput);
+        m_steeringMotor.set(steerOutput);
+    }
+
+
+    /** Zeros all the SwerveModule encoders. */
+    public void resetEncoders()
+    {
+        m_driveEncoder.setPosition(0);
+        m_steeringEncoder.setPosition(0);
+        
+    }
+
+}
