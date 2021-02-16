3b48fe5be22ad39b26a521f46fb54b52c5821ed0
the projrammers <frc5740@gmail.com>
2021-02-16 18:19:10 -0500

    Update SwerveModule.java

--- src/main/java/frc/robot/subsystems/SwerveModule.java
+++ src/main/java/frc/robot/subsystems/SwerveModule.java
@@ -10,7 +10,6 @@ import com.revrobotics.CANSparkMax;
 import com.revrobotics.ControlType;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
-
 import com.ctre.phoenix.sensors.CANCoder;
 import com.ctre.phoenix.sensors.CANCoderConfiguration;
 import com.ctre.phoenix.sensors.CANCoderFaults;
@@ -21,7 +20,9 @@ import edu.wpi.first.wpilibj.controller.PIDController;
 import edu.wpi.first.wpilibj.controller.ProfiledPIDController;
 import edu.wpi.first.wpilibj.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.kinematics.SwerveModuleState;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.trajectory.TrapezoidProfile;
+import frc.robot.Constants;
 import frc.robot.Constants.SwerveDriveModuleConstants;
 import frc.robot.Constants.SwerveDriveModuleConstants.DriveModulePIDValues;
 import frc.robot.Constants.SwerveDriveModuleConstants.SteeringControllerPIDValues;
@@ -62,6 +63,14 @@ public class SwerveModule
     //             SwerveDriveModuleConstants.k_MaxModuleAngularSpeedRadiansPerSecond,
     //             SwerveDriveModuleConstants.k_MaxModuleAngularAccelerationRadiansPerSecondSquared) );
     
+
+    public double kP_D, kI_D, kD_D, kIz_D, kFF_D, kMaxOutput_D, kMinOutput_D, maxRPM_D, maxVel_D, minVel_D, maxAcc_D, allowedErr_D;
+        
+    public double kP_S, kI_S, kD_S, kIz_S, kFF_S, kMaxOutput_S, kMinOutput_S, maxRPM_S, maxVel_S, minVel_S, maxAcc_S, allowedErr_S;
+
+ 
+
+
     /**
      * Constructs a SwerveModule.
      *
@@ -139,10 +148,23 @@ public class SwerveModule
     }
 
 
-    private void ConfigureDriveMotor(int channel, MotorType motorType)
+    public void ConfigureDriveMotor(int channel, MotorType motorType)
     {
         m_driveMotor = new CANSparkMax(channel, motorType);
-    
+
+        kP_D = DriveModulePIDValues.k_Proportional;
+        kI_D = DriveModulePIDValues.k_Intergral;
+        kD_D = DriveModulePIDValues.k_Differential;
+        kIz_D = DriveModulePIDValues.k_Iz;
+        kFF_D = DriveModulePIDValues.k_FF;
+        kMaxOutput_D = DriveModulePIDValues.k_MaxOutput;
+        kMinOutput_D = DriveModulePIDValues.k_MinOutput;
+        maxRPM_D = DriveModulePIDValues.k_maxRPM;
+        maxVel_D = DriveModulePIDValues.k_maxVel;
+        minVel_D = DriveModulePIDValues.k_minVel;
+        maxAcc_D = DriveModulePIDValues.k_maxAcc;
+        allowedErr_D = DriveModulePIDValues.k_allowedError;
+
         m_driveEncoder = m_driveMotor.getEncoder();   //assumes the use of the internal motor encoder
         //m_driveEncoder.setVelocityConversionFactor(SwerveDriveModuleConstants.k_DriveEncoderDistancePerPulse); // <- This line for use in WPILib PID loops, not Spark
         m_driverPIDController = m_driveMotor.getPIDController();
@@ -172,12 +194,47 @@ public class SwerveModule
         m_driverPIDController.setSmartMotionMinOutputVelocity(DriveModulePIDValues.k_minVel, DriveModulePIDValues.k_smartMotionSlot);
         m_driverPIDController.setSmartMotionMaxAccel(DriveModulePIDValues.k_maxAcc, DriveModulePIDValues.k_smartMotionSlot);
         m_driverPIDController.setSmartMotionAllowedClosedLoopError(DriveModulePIDValues.k_allowedError, DriveModulePIDValues.k_smartMotionSlot);
+      
+        // display PID coefficients on SmartDashboard
+        SmartDashboard.putNumber("P Gain", kP_D);
+        SmartDashboard.putNumber("I Gain", kI_D);
+        SmartDashboard.putNumber("D Gain", kD_D);
+        SmartDashboard.putNumber("I Zone", kIz_D);
+        SmartDashboard.putNumber("Feed Forward", kFF_D);
+        SmartDashboard.putNumber("Max Output", kMaxOutput_D);
+        SmartDashboard.putNumber("Min Output", kMinOutput_D);
+
+        // display Smart Motion coefficients
+        SmartDashboard.putNumber("Max Velocity", maxVel_D);
+        SmartDashboard.putNumber("Min Velocity", minVel_D);
+        SmartDashboard.putNumber("Max Acceleration", maxAcc_D);
+        SmartDashboard.putNumber("Allowed Closed Loop Error", allowedErr_D);
+        SmartDashboard.putNumber("Set Position", 0);
+        SmartDashboard.putNumber("Set Velocity", 0);
+        
+        // button to toggle between velocity and smart motion modes
+        SmartDashboard.putBoolean("Mode", true);
+
+        
     }
 
 
     private void ConfigureSteeringMotor(int channel, MotorType motorType)
     {
         m_steeringMotor = new CANSparkMax(channel, motorType);
+
+        kP_S = SteeringControllerPIDValues.k_Prop;
+        kI_S = SteeringControllerPIDValues.k_Inter;
+        kD_S = SteeringControllerPIDValues.k_Diff;
+        kIz_S = SteeringControllerPIDValues.k_Iz;
+        kFF_S = SteeringControllerPIDValues.k_FF;
+        kMaxOutput_S = SteeringControllerPIDValues.k_MaxOutput;
+        kMinOutput_S = SteeringControllerPIDValues.k_MinOutput;
+        maxRPM_S = SteeringControllerPIDValues.k_maxRPM;
+        maxVel_S = SteeringControllerPIDValues.k_maxVel;
+        minVel_S = SteeringControllerPIDValues.k_minVel;
+        maxAcc_S = SteeringControllerPIDValues.k_maxAcc;
+        allowedErr_S = SteeringControllerPIDValues.k_allowedError;
 
         // https://github.com/REVrobotics/SPARK-MAX-Examples/blob/master/Java/Alternate%20Encoder/src/main/java/frc/robot/Robot.java
         m_steeringEncoder = m_steeringMotor.getAlternateEncoder(SwerveDriveModuleConstants.k_AlternateEncoderType, SwerveDriveModuleConstants.k_AltEnc_CountPerRev);   // Using alternate external SRX Magnetic encoder
@@ -210,9 +267,27 @@ public class SwerveModule
         m_steeringPIDController.setSmartMotionMinOutputVelocity(SteeringControllerPIDValues.k_minVel, SteeringControllerPIDValues.k_smartMotionSlot);
         m_steeringPIDController.setSmartMotionMaxAccel(SteeringControllerPIDValues.k_maxAcc, SteeringControllerPIDValues.k_smartMotionSlot);
         m_steeringPIDController.setSmartMotionAllowedClosedLoopError(SteeringControllerPIDValues.k_allowedError, SteeringControllerPIDValues.k_smartMotionSlot);
+
+        // display PID coefficients on SmartDashboard
+        SmartDashboard.putNumber("P Gain", kP_S);
+        SmartDashboard.putNumber("I Gain", kI_S);
+        SmartDashboard.putNumber("D Gain", kD_S);
+        SmartDashboard.putNumber("I Zone", kIz_S);
+        SmartDashboard.putNumber("Feed Forward", kFF_S);
+        SmartDashboard.putNumber("Max Output", kMaxOutput_S);
+        SmartDashboard.putNumber("Min Output", kMinOutput_S);
+
+        // display Smart Motion coefficients
+        SmartDashboard.putNumber("Max Velocity", maxVel_S);
+        SmartDashboard.putNumber("Min Velocity", minVel_S);
+        SmartDashboard.putNumber("Max Acceleration", maxAcc_S);
+        SmartDashboard.putNumber("Allowed Closed Loop Error", allowedErr_S);
+        SmartDashboard.putNumber("Set Position", 0);
+        SmartDashboard.putNumber("Set Velocity", 0);
+        
+        // button to toggle between velocity and smart motion modes
+        SmartDashboard.putBoolean("Mode", true);
     }
-
-
 
 
 }
