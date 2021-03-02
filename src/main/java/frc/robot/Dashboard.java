03bacb486e2f839a582ffed6d300ed710cd1fd1b
Seth Peasley <seth.peasley@gmail.com>
2021-03-02 17:22:08 -0500

    Edits to fix code layout.

--- src/main/java/frc/robot/Dashboard.java
+++ src/main/java/frc/robot/Dashboard.java
@@ -11,47 +11,49 @@ import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardLayout;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj.shuffleboard.WidgetType;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.subsystems.DriveSubsystem;
 import frc.robot.subsystems.SwerveModule;
 
-public class Dashboard {
+public class Dashboard
+{
+    public DriveSubsystem driver;
+    public Constants constants;
+    public SwerveModule swerve;
 
-public DriveSubsystem driver;
-public Constants constants;
-public SwerveModule swerve;
+    private NetworkTableEntry gyro;
+    private NetworkTableEntry steeringPID;
+    private NetworkTableEntry drivePID;
 
-private NetworkTableEntry gyro;
-private NetworkTableEntry steeringPID;
-private NetworkTableEntry drivePID;
+    public Dashboard(DriveSubsystem m_DriveSubsystem, Constants m_Constants, SwerveModule m_SwerveModule)
+    {
+        this.driver = m_DriveSubsystem;
+        this.constants = m_Constants;
+        this.swerve = m_SwerveModule;
 
-public Dashboard(DriveSubsystem m_DriveSubsystem, Constants m_Constants, SwerveModule m_SwerveModule ){
-    this.driver = m_DriveSubsystem;
-    this.constants = m_Constants;
-    this.swerve = m_SwerveModule;
-
-    TeleopDashboard();
-}
-    
-
-
-public void TeleopDashboard() {
-
-final ShuffleboardTab Teleop_Dashboard = Shuffleboard.getTab("TeleopDash");
-
-this.drivePID = Teleop_Dashboard.add("Drive PID", 0).withWidget(BuiltInWidgets.kPIDController).getEntry();
-this.steeringPID = Teleop_Dashboard.add("Steering PID", 0).withWidget(BuiltInWidgets.kPIDController).getEntry();
-this.gyro = Teleop_Dashboard.add("Gyro", 0).withWidget(BuiltInWidgets.kGyro).getEntry();
-
-}
-
-public void dashboardData() throws NullPointerException {
-
-    try {
+        TeleopDashboard();
+    }
         
-    } catch (final Exception e) {
 
-    e.printStackTrace();
-}  
 
-}}
+    public void TeleopDashboard()
+    {
+        final ShuffleboardTab Teleop_Dashboard = Shuffleboard.getTab("TeleopDash");
+
+        this.drivePID = Teleop_Dashboard.add("Drive PID", 0).withWidget(BuiltInWidgets.kPIDController).getEntry();
+        this.steeringPID = Teleop_Dashboard.add("Steering PID", 0).withWidget(BuiltInWidgets.kPIDController).getEntry();
+        this.gyro = Teleop_Dashboard.add("Gyro", 0).withWidget(BuiltInWidgets.kGyro).getEntry();
+
+    }
+
+    public void dashboardData() throws NullPointerException 
+    {
+        try 
+        {
+            
+        }
+        catch (final Exception e) 
+        {
+            e.printStackTrace();
+        }
+    }
+}
--- src/main/java/frc/robot/commands/SwerveDriveCommand.java
+++ src/main/java/frc/robot/commands/SwerveDriveCommand.java
@@ -10,51 +10,49 @@ import frc.robot.subsystems.DriveSubsystem;
 import frc.robot.subsystems.SwerveModule;
 import frc.robot.Constants.SwerveDriveModuleConstants;
 
-public class SwerveDriveCommand extends CommandBase {
+public class SwerveDriveCommand extends CommandBase 
+{
+    private final DriveSubsystem drivetrain;
+    private final XboxController controller;
 
-private final DriveSubsystem drivetrain;
-private final XboxController controller;
+    private final SlewRateLimiter xspeedLimiter = new SlewRateLimiter(6);
+    private final SlewRateLimiter yspeedLimiter = new SlewRateLimiter(6);
+    private final SlewRateLimiter rotLimiter = new SlewRateLimiter(6);
 
-        
-private final SlewRateLimiter xspeedLimiter = new SlewRateLimiter(6);
-private final SlewRateLimiter yspeedLimiter = new SlewRateLimiter(6);
-private final SlewRateLimiter rotLimiter = new SlewRateLimiter(6);
+    public SwerveDriveCommand(DriveSubsystem drivetrain, XboxController controller)
+    {
+        this.drivetrain = drivetrain;
+        addRequirements(drivetrain);
 
+        this.controller = controller;
+    }
 
-public SwerveDriveCommand(DriveSubsystem drivetrain, XboxController controller) {
-    this.drivetrain = drivetrain;
-    addRequirements(drivetrain);
+    @Override
+    public void execute() 
+    {
+        // Get the x speed. We are inverting this because Xbox controllers return
+        // negative values when we push forward.
+        final var xSpeed =
+            xspeedLimiter.calculate(controller.getY(GenericHID.Hand.kLeft))
+            * SwerveDriveModuleConstants.kMaxSpeed;
 
-    this.controller = controller;
-}
+        // Get the y speed or sideways/strafe speed. We are inverting this because
+        // we want a positive value when we pull to the left. Xbox controllers
+        // return positive values when you pull to the right by default.
+        final var ySpeed =
+            yspeedLimiter.calculate(controller.getX(GenericHID.Hand.kLeft))
+            * SwerveDriveModuleConstants.kMaxSpeed;
 
-@Override
-public void execute() {
-  // Get the x speed. We are inverting this because Xbox controllers return
-  // negative values when we push forward.
-  final var xSpeed =
-    xspeedLimiter.calculate(controller.getY(GenericHID.Hand.kLeft))
-      * SwerveDriveModuleConstants.kMaxSpeed;
+        // Get the rate of angular rotation. We are inverting this because we want a
+        // positive value when we pull to the left (remember, CCW is positive in
+        // mathematics). Xbox controllers return positive values when you pull to
+        // the right by default.
+        final var rot =
+            -rotLimiter.calculate(controller.getX(GenericHID.Hand.kRight))
+            * SwerveDriveModuleConstants.kMaxAngularSpeed;
 
-  // Get the y speed or sideways/strafe speed. We are inverting this because
-  // we want a positive value when we pull to the left. Xbox controllers
-  // return positive values when you pull to the right by default.
-  final var ySpeed =
-    yspeedLimiter.calculate(controller.getX(GenericHID.Hand.kLeft))
-      * SwerveDriveModuleConstants.kMaxSpeed;
+        boolean calibrate = controller.getBumper(GenericHID.Hand.kLeft);
 
-  // Get the rate of angular rotation. We are inverting this because we want a
-  // positive value when we pull to the left (remember, CCW is positive in
-  // mathematics). Xbox controllers return positive values when you pull to
-  // the right by default.
-  final var rot =
-    -rotLimiter.calculate(controller.getX(GenericHID.Hand.kRight))
-      * SwerveDriveModuleConstants.kMaxAngularSpeed;
-
-  boolean calibrate = controller.getBumper(GenericHID.Hand.kLeft);
-
-  drivetrain.drive(xSpeed, ySpeed, rot, true, calibrate);
-  
-}
-
+        drivetrain.drive(xSpeed, ySpeed, rot, true, calibrate);
+    }
 }
