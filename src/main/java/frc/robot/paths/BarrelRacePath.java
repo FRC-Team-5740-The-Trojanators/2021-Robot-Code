d116c2fa83c10a20aec6deee06cc397eb6c33e02
Bridget47 <75648963+Bridget47@users.noreply.github.com>
2021-03-19 19:12:02 -0400

    Added Barrel Race Path

--- src/main/java/frc/robot/paths/BarrelRacePath.java
+++ src/main/java/frc/robot/paths/BarrelRacePath.java
@@ -25,7 +25,27 @@ public class BarrelRacePath
                                             // x    y      heading
     private static Pose2d start = new Pose2d(0.0, 0.0, new Rotation2d(Math.PI/2));
 
-    private static Pose2d point1 = new Pose2d(0, 0, Rotation2d.fromDegrees(0));
+    private static Pose2d point1 = new Pose2d(2.3, 1.5, Rotation2d.fromDegrees(0));
+    private static Pose2d point2 = new Pose2d(2.5, 2.8, Rotation2d.fromDegrees(0));
+    private static Pose2d point3 = new Pose2d(2.3, 3.8, Rotation2d.fromDegrees(0));
+    private static Pose2d point4 = new Pose2d(1.8, 4.2, Rotation2d.fromDegrees(0));
+    private static Pose2d point5 = new Pose2d(1.3, 4.3, Rotation2d.fromDegrees(0));
+    private static Pose2d point6 = new Pose2d(1, 3.8, Rotation2d.fromDegrees(0));
+    private static Pose2d point7 = new Pose2d(1.3, 3.3, Rotation2d.fromDegrees(0));
+    private static Pose2d point8 = new Pose2d(2, 3.6, Rotation2d.fromDegrees(0));
+    private static Pose2d point9 = new Pose2d(2.3, 4.3, Rotation2d.fromDegrees(0));
+    private static Pose2d point10 = new Pose2d(2.5, 5.6, Rotation2d.fromDegrees(0));
+    private static Pose2d point11 = new Pose2d(2.8, 6.6, Rotation2d.fromDegrees(0));
+    private static Pose2d point12 = new Pose2d(3.4, 6.6, Rotation2d.fromDegrees(0));
+    private static Pose2d point13 = new Pose2d(3.4, 5.3, Rotation2d.fromDegrees(0));
+    private static Pose2d point14 = new Pose2d(1.8, 6.1, Rotation2d.fromDegrees(0));
+    private static Pose2d point15 = new Pose2d(1.3, 7.4, Rotation2d.fromDegrees(0));
+    private static Pose2d point16 = new Pose2d(1.3, 8, Rotation2d.fromDegrees(0));
+    private static Pose2d point17 = new Pose2d(1.8, 8.4, Rotation2d.fromDegrees(0));
+    private static Pose2d point18 = new Pose2d(2.2, 8.1, Rotation2d.fromDegrees(0));
+    private static Pose2d point19 = new Pose2d(2.3, 7.9, Rotation2d.fromDegrees(0));
+    private static Pose2d point20 = new Pose2d(2.8, 4.1, Rotation2d.fromDegrees(0));
+    private static Pose2d point21 = new Pose2d(2.8, 1.5, Rotation2d.fromDegrees(0));
 
     private static Pose2d end =    new Pose2d(3.25, 3.00, new Rotation2d(Math.PI));
 
@@ -36,7 +56,7 @@ public class BarrelRacePath
     public static Trajectory getTrajectory()
     {
         return TrajectoryGenerator.generateTrajectory(
-            List.of(start, point1, end), 
+            List.of(start, point1, point2, point3, point4, point5, point6, point7, point8, point9, point10, point11, point12, point13, point14, point15, point16, point17, point18, point19, point20, point21, end), 
             trajectoryConfiguration);
     }
 
